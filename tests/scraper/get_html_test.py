6adac1d8ce1d81d0c768b14bbe020583548f82ea
Jason Green <justjasongreen@gmail.com>
2016-07-24 11:34:04 +1000

    Check HTTP status codes

--- tests/scraper/get_html_test.py
+++ tests/scraper/get_html_test.py
@@ -1,8 +1,14 @@
 import pytest
 
 
-def test_raises(scraper):
-    """The get_html method should raise an exception when receiving a HTTP error status code"""
+def test_4xx(scraper):
+    """The get_html method should return None when receiving a HTTP 4xx status code"""
+
+    assert scraper.get_html('http://httpstat.us/404') is None
+
+
+def test_5xx(scraper):
+    """The get_html method should raise an exception when receiving a HTTP 5xx status code"""
 
     with pytest.raises(BaseException):
         scraper.get_html('http://httpstat.us/500')
